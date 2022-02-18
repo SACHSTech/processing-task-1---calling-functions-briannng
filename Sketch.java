b53851f80bc3a09a938f68e1cca9f2214d21d9ed
Brian Gao <99832367+briannng@users.noreply.github.com>
2022-02-18 18:44:46 +0000

    I drew a house

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,13 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+    
+    
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
+    // put your size call here
     size(400, 400);
   }
 
@@ -16,21 +16,88 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(51,204,255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+      
+    //sun 
+    fill(255, 255, 0);
+    ellipse(50, 50, 80, 80);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //cloud
+    fill(255, 255, 255);
+    ellipse(190, 37, 140, 40);
+
+    //cloud
+    fill(255, 255, 255);
+    ellipse(290, 40, 140, 40);
+
+    //house
+    fill(255, 255, 153);
+    rect(100, 155, 200, 200);
+
+    //windows
+    fill(255, 255, 255);
+    rect(110, 280, 50, 50);
+
+      // Window
+    fill(255, 255, 255);
+    rect(238, 280, 50, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(233, 280, 60, 5);
+
+    // Window
+    fill(255, 255, 255);
+    rect(112, 200, 75, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(107, 200, 85, 5);
+
+    // Window
+    fill(255, 255, 255);
+    rect(213, 200, 75, 50);
+
+    // Window Roof
+    fill(51, 0, 0);
+    rect(208, 200, 85, 5);
+
+    // House Roof
+    fill(255, 153, 0);
+    triangle(200, 75, 325, 175, 75, 175);
+
+    // Tree Leaves
+    fill(0, 255, 51);
+    ellipse(65, 220, 100, 100);
+
+    // Tree Trunk
+    fill(102, 51, 0);
+    rect(55, 270, 20, 100);
+
+   // Bush
+    fill(0, 153, 0);
+    ellipse(315, 330, 60, 60);
+
+    // Bush
+    fill(0, 102, 0);
+    ellipse(340, 350, 60, 60);
+
+    // Bush
+    fill(0, 204,0);
+    ellipse(300, 350, 60, 60);
+
+    //grass
+    fill(0, 204, 0);
+    rect(0, 380, 400, 50);
+
   }
-  
-  // define other methods down here.
 }
+
+
+
